66b3219a2f524ecd1b42b04f392c1ddf4ea66783
leadsoftengineer <multisoftbox@gmail.com>
2019-12-11 08:18:02 +0200

    components commit

--- manager/frontend/src/components/App.js
+++ manager/frontend/src/components/App.js
@@ -1,11 +1,20 @@
 //KYIV MEDIA 10.12.2019
-import React , { Component }from 'react';
-import ReactDom from 'react-dom';
+import React, { Component, Fragment } from "react";
+import ReactDom from "react-dom";
+import Header from "./layout/Header";
+import Dashboard from "../components/leads/Dashboard";
 
 class App extends React.Component {
-   render(){
-       return <h1>React App</h1>
-   }
+  render() {
+    return (
+      <Fragment>
+        <Header />
+        <div className="container">
+          <Dashboard />
+        </div>
+      </Fragment>
+    );
+  }
 }
 
-ReactDom.render(<App />, document.getElementById('app'));
+ReactDom.render(<App />, document.getElementById("app"));
---
+++ manager/frontend/src/components/leads/Dashboard.js
@@ -0,0 +1,13 @@
+//KYIV MEDIA 11.12.2019
+import React, { Fragment } from "react";
+import Form from "./Form";
+import Leads from "./Leads";
+
+export default function Dashboard() {
+  return (
+    <Fragment>
+      <Form />
+      <Leads />
+    </Fragment>
+  );
+}
