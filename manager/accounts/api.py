d5c88ce926e3df3ee17e163ae082f842ca194ea8
leadsoftengineer <multisoftbox@gmail.com>
2019-12-23 08:28:41 +0200

    user commit

---
+++ manager/accounts/api.py
@@ -0,0 +1,11 @@
+#KYIV MEDIA 23.12.2019
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from knox.models import AuthToken
+from .serializers import UserSerializer,  RegisterSerializer
+
+# Register API
+
+# Login API
+
+# Get User API
--- manager/frontend/src/actions/leads.js
+++ manager/frontend/src/actions/leads.js
@@ -1,7 +1,7 @@
 //KYIV MEDIA 18.12.2019
 import axios from "axios";
-import { createMessage } from "./messages";
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from "./types";
+import { createMessage, returnErrors } from "./messages";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
 //GET LEADS
 
@@ -14,7 +14,9 @@ export const getLeads = () => dispatch => {
         payload: res.data
       });
     })
-    .catch(err => console.log(err));
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
 
 //DELETE LEAD
@@ -44,14 +46,7 @@ export const addLead = lead => dispatch => {
         payload: res.data
       });
     })
-    .catch(err => {
-      const errors = {
-        msg: err.response.data,
-        status: err.response.status
-      };
-      dispatch({
-        type: GET_ERRORS,
-        payload: errors
-      });
-    });
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
